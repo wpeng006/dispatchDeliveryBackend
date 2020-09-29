c72c0ff9967f2907429914f206071684dae9554d
Terrill <terrill@Terrills-MacBook-Pro.local>
2020-09-28 21:39:08 -0700

    Update for GoogleMapAPIClient

--- src/main/java/External/DirectionsResponse.java
+++ src/main/java/External/DirectionsResponse.java
@@ -1,8 +1,14 @@
 package External;
+import com.google.maps.model.LatLng;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.oracle.javafx.jmx.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import utils.Constants;
 
 
@@ -10,14 +16,14 @@ public class DirectionsResponse {
 
     private JsonElement jsonElement;
     private JsonObject jsonObject;
-    private JsonArray jsonObjectResult;
+    private JsonObject jsonObjectResult;
 
     public DirectionsResponse(String json) {
         jsonElement = new JsonParser().parse(json);
         jsonObject = jsonElement.getAsJsonObject();
 
-        // Get route array and pick up the first array by default
-        jsonObjectResult = jsonObject.getAsJsonArray("routes").get(0).getAsJsonArray("legs").get(0).getAsJsonObject();
+        // Get leg. There is no Waypoint so we just have one leg here
+        jsonObjectResult = jsonObject.getAsJsonArray("routes").get(0).getAsJsonObject().getAsJsonArray("legs").get(0).getAsJsonObject();
     }
 
     // Method for getting start_address
@@ -33,30 +39,155 @@ public class DirectionsResponse {
     }
 
     // Method for getting robotDuration
-    public double getRobotDuration() {
-        String duration = jsonObjectResult.get("duration").getAsObject().get("value").getAsString();
-        double durationValue = Long.parseLong(duration);
+    public long getRobotDuration() {
+        String duration = jsonObjectResult.getAsJsonObject("duration").get("value").getAsString();
+        long durationValue = Long.parseLong(duration);
         return durationValue;
     }
 
     // Method for getting robotDistance
     public long getRobotDistance() {
-        String robotDistance = jsonObjectResult.get("distance").getAsObject().get("value").getAsString();
+        String robotDistance = jsonObjectResult.getAsJsonObject("distance").get("value").getAsString();
         long robotDistanceValue = Long.parseLong(robotDistance);
         return robotDistanceValue;
     }
 
     // Method for getting droneDuration
-    public double getDroneDuration() {
+    public long getDroneDuration() {
         long droneDistanceValue = getDroneDistance();
-        return droneDistanceValue / Constants.DEFAULT_DRONE_SPEED;
+        return droneDistanceValue / 80;
     }
 
     // Method for getting droneDistance
-    public long getDroneDistance() {
-        String droneDistance = jsonObjectResult.get("distance").getAsObject().get("value").getAsString();
+/*    public long getDroneDistance() {
+        String droneDistance = jsonObjectResult.getAsJsonObject("distance").get("value").getAsString();
         long droneDistanceValue = Long.parseLong(droneDistance);
         return droneDistanceValue;
+    }*/
+
+    // Method for getting droneDistance (Approximately)
+    public double getDroneDistance() {
+        double startLat = Double.parseDouble(jsonObjectResult.getAsJsonObject("start_location").get("lat").getAsString());
+        double startLng = Double.parseDouble(jsonObjectResult.getAsJsonObject("start_location").get("lng").getAsString());
+
+        double endLat = Double.parseDouble(jsonObjectResult.getAsJsonObject("end_location").get("lat").getAsString());
+        double endLng = Double.parseDouble(jsonObjectResult.getAsJsonObject("end_location").get("lng").getAsString());
+
+        LatLng a = new LatLng(startLat, startLng);
+        LatLng b = new LatLng(endLat, endLng);
+
+        return Math.sqrt(Math.pow(a.lat - b.lat, 2)+Math.pow(a.lng - b.lng, 2));
+    }
+
+    /** Receives a JSONObject and returns a list of lists containing latitude and longitude */
+    public List<LatLng> getPoints(){
+        int SAMPLE_INTERVAL = 300; // Sample a point every 100 points.
+
+        // routes result
+        List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String, String>>>() ;
+
+        // Sampling points on a route btw source and dest.
+        List<LatLng> points = new ArrayList<>();
+
+        JsonArray jRoutes = null;
+        JsonArray jLegs = null;
+        JsonArray jSteps = null;
+
+        try {
+
+            // jRoutes = jObject.getJSONArray("routes");
+            jRoutes = jsonObject.getAsJsonArray("routes");
+
+            /** Traversing all routes */
+            for (int i = 0; i < jRoutes.size(); i++){
+                jLegs = ((JsonObject)jRoutes.get(i)).getAsJsonArray("legs");
+                List path = new ArrayList<HashMap<String, String>>();
+
+                /** Traversing all legs */
+                for (int j = 0; j<jLegs.size(); j++){
+                    jSteps = ((JsonObject)jLegs.get(j)).getAsJsonArray("steps");
+
+                    /** Traversing all steps */
+                    for (int k = 0; k < jSteps.size(); k++){
+                        String polyline = "";
+                        //polyline = (String)(((JsonObject)((JsonObject)jSteps.get(k)).get("polyline")).get("points"));
+                        polyline = ((JsonObject)(((JsonObject)jSteps.get(k)).get("polyline"))).get("points").getAsString();
+
+                        List list = decodePoly(polyline);
+
+                        /** Traversing all points */
+                        for (int l = 0; l < list.size(); l++) {
+                            HashMap<String, String> hm = new HashMap<String, String>();
+                            hm.put("lat", Double.toString(((LatLng)list.get(l)).lat) );
+                            hm.put("lng", Double.toString(((LatLng)list.get(l)).lng) );
+                            path.add(hm);
+                        }
+                    }
+                    routes.add(path);
+                }
+            }
+
+            for (int i = 0; i < routes.size(); i++) {
+
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    // 13.7 mile has 863 points. 100 points represents for 1.58 mile.
+                    if (j % SAMPLE_INTERVAL == 0) { // sample at every 1.58 mile
+                        HashMap<String, String> point = path.get(j);
+
+                        double lat = Double.parseDouble(point.get("lat"));
+                        double lng = Double.parseDouble(point.get("lng"));
+                        LatLng position = new LatLng(lat, lng);
+
+                        points.add(position);
+                    }
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+        }
+
+        return points;
+    }
+
+    /**
+     * Method to decode polyline points
+     * Courtesy : http://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
+     * */
+    private List decodePoly(String encoded) {
+
+        List poly = new ArrayList();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+        return poly;
     }
 }
 
